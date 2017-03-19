996d42f351f4fb7237d667fca4e79b46af18085f
Emmax77 <emmanuelguzman96@gmail.com>
2017-03-18 22:02:42 -0600

    Se crea la matriz de botones

--- Matrices/Form1.Designer.vb
+++ Matrices/Form1.Designer.vb
@@ -22,9 +22,39 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btNuevoJuego = New System.Windows.Forms.Button()
+        Me.plMat = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'btNuevoJuego
+        '
+        Me.btNuevoJuego.Location = New System.Drawing.Point(150, 31)
+        Me.btNuevoJuego.Name = "btNuevoJuego"
+        Me.btNuevoJuego.Size = New System.Drawing.Size(102, 23)
+        Me.btNuevoJuego.TabIndex = 0
+        Me.btNuevoJuego.Text = "Nuevo Juego"
+        Me.btNuevoJuego.UseVisualStyleBackColor = True
+        '
+        'plMat
+        '
+        Me.plMat.Location = New System.Drawing.Point(12, 78)
+        Me.plMat.Name = "plMat"
+        Me.plMat.Size = New System.Drawing.Size(710, 400)
+        Me.plMat.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(726, 482)
+        Me.Controls.Add(Me.plMat)
+        Me.Controls.Add(Me.btNuevoJuego)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents btNuevoJuego As Button
+    Friend WithEvents plMat As Panel
 End Class
--- Matrices/Form1.vb
+++ Matrices/Form1.vb
@@ -1,3 +1,37 @@
 ﻿Public Class Form1
 
+    Private Sub btNuevoJuego_Click(sender As Object, e As EventArgs) Handles btNuevoJuego.Click
+        hacerMatriz(19, 14)
+    End Sub
+
+    'Metodo para crear una matriz con numeros random
+    Sub hacerMatriz(num As Integer, num2 As Integer)
+        Dim mat(num, num2) As Button
+        Dim cont As Integer = 0
+        Dim posicionAncho As Integer = 0
+        Dim posicionAlto As Integer = 0
+
+        For i = 0 To mat.GetUpperBound(0)
+            For j = 0 To mat.GetUpperBound(1)
+                cont += 1
+                Dim aleatorio As Integer = CInt((2 * Rnd()) + 1)
+                mat(i, j) = New Button
+                mat(i, j).Name = CStr("bt" & cont)
+                mat(i, j).Text = aleatorio 'se cambia por l imagen del patito
+                mat(i, j).Width = 20
+                mat(i, j).Height = 20
+                mat(i, j).Left = posicionAncho
+                mat(i, j).Top = posicionAlto
+                posicionAncho += 20
+            Next
+            posicionAncho = 0
+            posicionAlto += 20
+        Next
+        For k = 0 To mat.GetUpperBound(0)
+            For l = 0 To mat.GetUpperBound(1)
+                plMat.Controls.Add(mat(k, l))
+            Next
+        Next
+
+    End Sub
 End Class
